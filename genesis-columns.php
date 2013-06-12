c51ff95717cc2de250058b71f8181cb2d576c311
Code96 <Code96@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-12 17:43:55 +0000

    Tagging version 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/genesis-easy-columns/tags/1.1.1@725799 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- genesis-columns.php
+++ genesis-columns.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Genesis Easy Columns
-Plugin URI: http://www.code96wd.com/genesis-easy-columns
+Plugin URI: http://www.code96wd.com/wordpress-plugins/
 Description: Easily add Genesis column shortcodes to your WordPress editor.
-Version: 1.1
+Version: 1.1.1
 Author: Matthew Smith
 Author URI: http://www.code96wd.com
 License: GPL2
