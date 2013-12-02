95364f66bc5eb48143b2e91747f24137366ea64b
Code96 <Code96@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-02 01:54:56 +0000

    Bug Fix
    git-svn-id: https://plugins.svn.wordpress.org/genesis-easy-columns/tags/1.3@813432 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,56 +1,60 @@
 <?php
+
 	//Turn editor auto format off
-	remove_filter( 'the_content', 'wpautop' );
+	$wpautop_on = has_filter('the_content', 'wpautop');
+	if ($wpautop_on) {
+		remove_filter( 'the_content', 'wpautop' );
+	}
 	
 	//Column Shortcodes
 	
 	function gc_one_half_first($atts, $content = null ) {
-	return '<div class="one-half first">'.$content.'</div>';
+	return '<div class="one-half first">'.do_shortcode($content).'</div>';
 	}
 	add_shortcode("one-half-first","gc_one_half_first");
 
 	function gc_one_half($atts, $content = null) {
-	return '<div class="one-half">'.$content.'</div>';
+	return '<div class="one-half">'.do_shortcode($content).'</div>';
 	}
 	add_shortcode('one-half','gc_one_half');
 
 	function gc_one_third_first($atts, $content = null) {
-	return '<div class="one-third first">' .$content. '</div>';
+	return '<div class="one-third first">' .do_shortcode($content). '</div>';
 	}
 	add_shortcode("one-third-first","gc_one_third_first");
 
 	function gc_one_third($atts, $content = null) {
-	return '<div class="one-third">' .$content. '</div>';
+	return '<div class="one-third">' .do_shortcode($content). '</div>';
 		}
 	add_shortcode('one-third','gc_one_third');
 
 	function gc_one_fourth_first($atts, $content = null) {
-	return '<div class="one-fourth first">' .$content. '</div>';
+	return '<div class="one-fourth first">' .do_shortcode($content). '</div>';
 	}
 	add_shortcode("one-fourth-first","gc_one_fourth_first");
 
 	function gc_one_fourth($atts, $content = null) {
-	return '<div class="one-fourth">' .$content. '</div>';
+	return '<div class="one-fourth">' .do_shortcode($content). '</div>';
 	}
 	add_shortcode('one-fourth','gc_one_fourth');
 
 	function gc_one_fifth_first($atts, $content = null) {
-	return '<div class="one-fifth first">' .$content. '</div>';
+	return '<div class="one-fifth first">' .do_shortcode($content). '</div>';
 	}
 	add_shortcode("one-fifth-first","gc_one_fifth_first");
 
 	function gc_one_fifth($atts, $content = null) {
-	return '<div class="one-fifth">' .$content. '</div>';
+	return '<div class="one-fifth">' .do_shortcode($content). '</div>';
 	}
 	add_shortcode('one-fifth','gc_one_fifth');
 
 	function gc_one_sixth_first($atts, $content = null) {
-	return '<div class="one-sixth first">' .$content. '</div>';
+	return '<div class="one-sixth first">' .do_shortcode($content). '</div>';
 	}
 	add_shortcode("one-sixth-first","gc_one_sixth_first");
 
 	function gc_one_sixth($atts, $content = null) {
-	return '<div class="one-sixth">' .$content. '</div>';
+	return '<div class="one-sixth">' .do_shortcode($content). '</div>';
 	}
 	add_shortcode('one-sixth','gc_one_sixth');
 	
@@ -66,4 +70,6 @@
 	
 
 	//Turn auto format back on
-	add_filter( 'the_content', 'wpautop' , 12);
+	if ($wpautop_on) {
+		add_filter( 'the_content', 'wpautop', (is_int($wpautop_on) ? $wpautop_on : 10));
+	}
